699d219f0f6d6ac5e0d054cfe2085b3b137b8122
Mikekoropsak <126094021+Mikekoropsak@users.noreply.github.com>
2026-03-02 14:18:37 -0500

    Update eda.py

--- eda.py
+++ eda.py
@@ -3,6 +3,13 @@ import numpy as np
 import matplotlib.pylot as plt
 import seaborn as sns
 
+### Check for empty rows ###
+if len(df.isna().any()[df.isna().any()]) == 0:
+    print('There are no empty columns')
+else: 
+    print(f'Columns with na: {df.columns[df.isna().any()].to_list()}')
+
+### Creat histogram and box plots for numerical columns ###
 def create_plots_num(df):
 
     num_cols = df.select_dtypes(include=np.number).columns
@@ -25,6 +32,7 @@ def create_plots_num(df):
 
 create_plots_num(df)
 
+### Create countplots for categorical columns ###
 def create_plots_cat(df):
     num_cols = df.select_dtypes(include=object).columns
     plt.figure(figsize=(10,8))
